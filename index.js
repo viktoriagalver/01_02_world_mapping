d614dc90d546f2124d22b38694040215cca3b3a4
viktoriagalver <56873841+viktoriagalver@users.noreply.github.com>
2019-12-03 15:39:01 +0100

    Überarbeitung

--- index.js
+++ index.js
@@ -21,30 +21,62 @@
  *  Aufgabe 2.1: Farbe! Schaut euch das npm-Modul "chalk" an, und versucht die Zeichen in Farbe auszugeben
  *
  */
-
 const rl = require('readline')
-const jimp = require('jimp')
+const chalk = require('chalk');
+  //const jimp = require('jimp')
 
 clearConsole()
 
-jimp.read('world.jpg', (err, map_image) => {
+/*jimp.read('world.jpg', (err, map_image) => {
   if (err) throw err;
-  map_image.resize(100,50);
-  console.log(map_image.getPixelColor(0,0)); //gibt die Farbe des Pixels links oben zurück
-  writeCharacterToConsole('#',0,0); //schreibt ein # links oben in die Konsole
+  map_image.resize(170,50);
+  //console.log(map_image.getPixelColor(0,0)); //gibt die Farbe des Pixels links oben zurück
+  //writeCharacterToConsole('#',0,0); //schreibt ein # links oben in die Konsole
 
   //----- Hier kommt euer Code hin -----
-  
+  //wenn pixel weiß --> nichts
+  //wenn pixel nicht weiß --> # 
 
+  const chalk = require('chalk');
+  // for (let i = 0; i < 170; i++) {
+  //   for (let u = 0; u < 50; u++) {
+  //     let pixelColor = map_image.getPixelColor(i,u)
+  //     if (pixelColor === 255) {
+  //       writeCharacterToConsole(chalk.red('#'),i,u);
+  //     }
+  //   }
+  // }
+  */
 
-});
+function generateInvader (width, height, xpos, ypos) {
 
+  for (let i = 0; i < width; i++) {
+    for (let u = 0; u < height; u++) {
 
+      const hue = Math.random() * 360;
+
+      let number = Math.random();
+
+      let r = Math.floor(Math.random() * 100);
+      let g = Math.floor(Math.random() * 100);
+      // let b = Math.floor(Math.random() * 256);
+
+      if (number >= 0.5) {
+        writeCharacterToConsole(chalk.hsv(hue, r, g)('▄'), i + xpos, u + ypos);
+        writeCharacterToConsole(chalk.hsv(hue, r, g)('▄'), (2 * width) - i + xpos, u + ypos);
+      }
+    }
+  }
+}
 
 //Vorerst nur ein Platzhalter
 setInterval(function () {
-    
-},1000);
+  const xpos = Math.floor(Math.random() * 100);
+  const ypos = Math.floor(Math.random() * 40)
+  clearConsole();
+  generateInvader(10, 10, xpos, ypos);
+
+}, 500);
 
 
 /*
@@ -59,6 +91,6 @@ function clearConsole () {
 }
 
 function writeCharacterToConsole (char, x, y) {
-  rl.cursorTo(process.stdout,x,y)
+  rl.cursorTo(process.stdout, x, y)
   process.stdout.write(char)
 }
